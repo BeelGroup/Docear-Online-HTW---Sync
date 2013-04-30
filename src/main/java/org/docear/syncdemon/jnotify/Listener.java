73b24d864cf0d81f8076842c0eee316d4e1f1bc0
Michael Schleichardt <michael@schleichardt.net>
2013-04-30 07:36:09 +0200

    added log statements for all jnotify events

--- src/main/java/org/docear/syncdemon/jnotify/Listener.java
+++ src/main/java/org/docear/syncdemon/jnotify/Listener.java
@@ -4,10 +4,13 @@ import akka.actor.ActorRef;
 import net.contentobjects.jnotify.JNotifyListener;
 import org.docear.syncdemon.messages.FileCreatedMessage;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class Listener implements JNotifyListener {
+    private static final Logger logger = LoggerFactory.getLogger(Listener.class);
 
     final ActorRef recipient;
 
@@ -17,6 +20,7 @@ public class Listener implements JNotifyListener {
 
     @Override
     public void fileCreated(final int wd, final String rootPath, final String name) {
+        logger.debug("fileCreated {}/{}", rootPath, name);
         recipient.tell(new FileCreatedMessage() {
             final DateTime timestamp = DateTime.now();
             final String path = rootPath + File.separator + name;
@@ -40,13 +44,16 @@ public class Listener implements JNotifyListener {
 
     @Override
     public void fileDeleted(final int wd, final String rootPath, final String name) {
+        logger.debug("fileDeleted {}/{}", rootPath, name);
     }
 
     @Override
     public void fileModified(final int wd, final String rootPath, final String name) {
+        logger.debug("fileModified {}/{}", rootPath, name);
     }
 
     @Override
     public void fileRenamed(final int wd, final String rootPath, final String oldName, final String newName) {
+        logger.debug("fileRenamed rootpath={}, oldName={}, newName={}", rootPath, oldName, newName);
     }
 }
