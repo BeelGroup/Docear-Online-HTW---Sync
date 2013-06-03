37c8614fa7b4aca40a45b86ce90c86f695ada9fe
Michael Schleichardt <michael@schleichardt.net>
2013-06-03 08:19:32 +0200

    disabled SNAPSHOT releases

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,12 @@ object Build extends Build {
       publishMavenStyle := true
       , sources in (Compile, doc) ~= (_ filter (f => false)) //sbt publish does not work with Java settings, i.e.: javadoc: error - invalid flag: -target
       , publishArtifact in Test := false
-      , publishTo := releaseFolder //Please provide a release directory with `sbt -Drelease.folder=<path> publish`.
+      , publishTo <<= version { (v: String) =>
+        if (v.trim.endsWith("SNAPSHOT"))
+          None //Please publish SNAPSHOT releases only to a local repository with `sbt publish-local`.
+        else
+          releaseFolder //Please provide a release directory with `sbt -Drelease.folder=<path> publish`.
+      }
       , pomIncludeRepository := { _ => false }
     )
   }
