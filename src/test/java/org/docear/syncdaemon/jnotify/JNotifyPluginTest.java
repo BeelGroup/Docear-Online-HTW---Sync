efc28dd75a50b266bdb20829d92740c4e7421807
Michael Schleichardt <michael@schleichardt.net>
2013-06-04 14:27:12 +0200

    ignore jnotify test, CI server needs libraries

--- src/test/java/org/docear/syncdaemon/jnotify/JNotifyPluginTest.java
+++ src/test/java/org/docear/syncdaemon/jnotify/JNotifyPluginTest.java
@@ -4,6 +4,7 @@ import net.contentobjects.jnotify.JNotify;
 import net.contentobjects.jnotify.JNotifyListener;
 import org.apache.commons.io.FileUtils;
 import org.docear.syncdaemon.Daemon;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,6 +14,7 @@ import static org.fest.assertions.Assertions.assertThat;
 
 public class JNotifyPluginTest {
     @Test
+    @Ignore
     public void testJNotifyIsActive() throws Exception {
         final Daemon daemon = testDaemon();
         daemon.onStart();
