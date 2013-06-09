a65cb0c86471ee761d11588832223370d4b45720
Michael Schleichardt <michael@schleichardt.net>
2013-06-09 10:10:47 +0200

    refactoring dependency configuration, extract variables for jersey, jackson and logback version

--- project/Build.scala
+++ project/Build.scala
@@ -6,28 +6,33 @@ import sbt.Tests.Setup
 
 object Build extends Build {
   
-  val dependencies = Seq(
-    "net.contentobjects.jnotify" % "jnotify" % "0.94" //watches changes of files, uses native libraries
-    , "com.typesafe.akka" % "akka-actor_2.10" % "2.1.2" //event-driven programming with messages and actors
-    , "joda-time" % "joda-time" % "2.2" //library for timing
-    , "com.google.guava" % "guava" % "14.0.1" //functional programming and utils for collections
-    , "org.apache.commons" % "commons-lang3" % "3.1" //functions missing in the Java library
-    , "commons-io" % "commons-io" % "2.4" //file and stream operations
-    , "org.slf4j" % "slf4j-api" % "1.7.5" //logging. logger for classes
-    , "ch.qos.logback" % "logback-classic" % "1.0.12" //configuration of logging
-    , "ch.qos.logback" % "logback-core" % "1.0.12" //configuration of logging
-    , "uk.org.lidalia" % "sysout-over-slf4j" % "1.0.2" //log System.{out,err}.println to logback
-    , "com.novocode" % "junit-interface" % "0.8" % "test->default" //needed by JUnit with SBT
-    , "junit" % "junit" % "4.11" % "test" //test suite
-    , "org.easytesting" % "fest-assert" % "1.4" % "test"//assertions with better messages
-    , "com.sun.jersey" % "jersey-core" % "1.17.1"
-    , "com.sun.jersey" % "jersey-client" % "1.17.1"
-    , "com.sun.jersey.contribs" % "jersey-apache-client" % "1.17.1"
-    ,"com.fasterxml.jackson.core" % "jackson-core" % "2.1.4"
-	,"com.fasterxml.jackson.core" % "jackson-annotations" % "2.1.4"
-    ,"com.fasterxml.jackson.core" % "jackson-databind" % "2.1.4"
-            
-  )
+  val dependencies = {
+
+    val jerseyVersion = "1.17.1"
+    val jacksonVersion = "2.1.4"
+    val logbackVersion = "1.0.12"
+    Seq(
+      "net.contentobjects.jnotify" % "jnotify" % "0.94" //watches changes of files, uses native libraries
+      , "com.typesafe.akka" % "akka-actor_2.10" % "2.1.2" //event-driven programming with messages and actors
+      , "joda-time" % "joda-time" % "2.2" //library for timing
+      , "com.google.guava" % "guava" % "14.0.1" //functional programming and utils for collections
+      , "org.apache.commons" % "commons-lang3" % "3.1" //functions missing in the Java library
+      , "commons-io" % "commons-io" % "2.4" //file and stream operations
+      , "org.slf4j" % "slf4j-api" % "1.7.5" //logging. logger for classes
+      , "ch.qos.logback" % "logback-classic" % logbackVersion //configuration of logging
+      , "ch.qos.logback" % "logback-core" % logbackVersion //configuration of logging
+      , "uk.org.lidalia" % "sysout-over-slf4j" % "1.0.2" //log System.{out,err}.println to logback
+      , "com.novocode" % "junit-interface" % "0.8" % "test->default" //needed by JUnit with SBT
+      , "junit" % "junit" % "4.11" % "test" //test suite
+      , "org.easytesting" % "fest-assert" % "1.4" % "test"//assertions with better messages
+      , "com.sun.jersey" % "jersey-core" % jerseyVersion
+      , "com.sun.jersey" % "jersey-client" % jerseyVersion
+      , "com.sun.jersey.contribs" % "jersey-apache-client" % jerseyVersion
+      , "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion
+      , "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonVersion
+      , "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion
+    )
+  }
 
   lazy val projectReleaseSettings = {
     lazy val releaseFolder = Option(System.getProperty("release.folder")).map(x => Resolver.file("file", new File(x)))
