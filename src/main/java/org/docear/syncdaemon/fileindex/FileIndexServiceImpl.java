8c935fd47015fccf93bf95ea7bdd777ce793ab77
Michael Schleichardt <michael@schleichardt.net>
2013-06-09 11:09:55 +0200

    Merge branch 'db'
    Conflicts:
    	src/main/java/org/docear/syncdaemon/fileindex/FileIndexServiceImpl.java

--- src/main/java/org/docear/syncdaemon/fileindex/FileIndexServiceImpl.java
+++ src/main/java/org/docear/syncdaemon/fileindex/FileIndexServiceImpl.java
@@ -2,39 +2,48 @@ package org.docear.syncdaemon.fileindex;
 
 import java.util.List;
 
-import org.docear.syncdaemon.actors.Service;
-import org.docear.syncdaemon.client.ClientService;
-import org.docear.syncdaemon.client.ClientServiceImpl;
 import org.docear.syncdaemon.indexdb.IndexDbService;
 import org.docear.syncdaemon.indexdb.IndexDbServiceImpl;
 import org.docear.syncdaemon.indexdb.PersistenceException;
+import org.docear.syncdaemon.messages.FileChangeEvent;
+import org.docear.syncdaemon.messages.FileConflictEvent;
 import org.docear.syncdaemon.projects.Project;
 
 import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FileIndexServiceImpl extends Service implements FileIndexService {
+public class FileIndexServiceImpl extends UntypedActor implements
+        FileIndexService {
     private static final Logger logger = LoggerFactory.getLogger(FileIndexServiceImpl.class);
-	private final ClientService clientService;
-	private final IndexDbService indexDbService;
-	
-	public FileIndexServiceImpl(ActorRef recipient, Project project){
-		super(recipient, project);
-		clientService = new ClientServiceImpl();
-		indexDbService = new IndexDbServiceImpl();
-	}
-	
-	@Override
-	public void scanProject() {
+
+    private ActorRef recipient;
+    private Project project;
+    private final IndexDbService indexDbService;
+
+    public FileIndexServiceImpl() {
+        indexDbService = new IndexDbServiceImpl();
+    }
+
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof StartScanMessage) {
+            this.project = ((StartScanMessage) message).getProject();
+            this.recipient = ((StartScanMessage) message).getServerSynchronisationActor();
+            scanProject();
+        }
+    }
+
+    @Override
+    public void scanProject() {
         try {
             long localRev = indexDbService.getProjectRevision(project.getId());
 
-            if (localRev != project.getRevision()){
+            if (localRev != project.getRevision()) {
                 List<FileMetaData> files = FileReceiver.receiveFiles(project);
 
                 // TODO user credentials required to use clientService
-                for (FileMetaData fmdFromScan : files){
+                for (FileMetaData fmdFromScan : files) {
                     final FileMetaData fmdFromIndexDb = indexDbService.getFileMetaData(fmdFromScan);
                     if (fmdFromScan.isChanged(fmdFromIndexDb)) {
                         sendConflictMesage(fmdFromScan);
@@ -44,8 +53,18 @@ public class FileIndexServiceImpl extends Service implements FileIndexService {
                 }
             }
         } catch (PersistenceException e) {
-            logger.error("can't scan project", e);
+            logger.error("can't scan projects", e);
         }
+    }
+
+    private void sendConflictMesage(final FileMetaData fmd) {
+        final FileConflictEvent message = new FileConflictEvent(fmd.getPath(),project.getId());
+        recipient.tell(message, recipient);
+    }
+
+    private void sendFileChangedMessage(final FileMetaData fmd) {
+        final FileChangeEvent message = new FileChangeEvent(fmd.getPath(),project.getId());
+        recipient.tell(message, recipient);
     }
 
 }
