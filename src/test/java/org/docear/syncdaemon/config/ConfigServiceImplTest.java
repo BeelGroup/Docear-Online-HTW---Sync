03051ff1634fb7c36a1b40ec53c07b789282f00c
AlexSchwank <alschwank@gmail.com>
2013-06-22 00:21:16 +0200

    add save test for configService. Updated ProjectCollection and Project for jackson xml generation.

--- src/test/java/org/docear/syncdaemon/config/ConfigServiceImplTest.java
+++ src/test/java/org/docear/syncdaemon/config/ConfigServiceImplTest.java
@@ -1,19 +1,29 @@
 package org.docear.syncdaemon.config;
 
-import org.docear.syncdaemon.Daemon;
-import org.docear.syncdaemon.projects.Project;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.apache.commons.io.FileUtils.getTempDirectoryPath;
 import static org.docear.syncdaemon.TestUtils.testDaemon;
 import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+
+import org.docear.syncdaemon.Daemon;
+import org.docear.syncdaemon.projects.Project;
+import org.docear.syncdaemon.projects.ProjectCollection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class ConfigServiceImplTest {
     private ConfigService service;
     private Daemon daemon;
 
+    private static final Logger logger = LoggerFactory.getLogger(ConfigServiceImplTest.class);
+    
     @Before
     public void setUp() throws Exception {
         daemon = testDaemon();
@@ -51,5 +61,21 @@ public class ConfigServiceImplTest {
     	service.deleteProject(project);
     	assertThat(service.getProjects().size()).isEqualTo(0);
     	assertThat(service.getProjectRootPath(project.getId())).isNull();
+    }
+    
+    @Test
+    public void saveProject() throws Exception {
+    	Project project = new Project("thisIsTheId", "/root/Path", 8L);
+    	service.addProject(project);
+    	service.saveConfig();
+    	XmlMapper xmlMapper = new XmlMapper();
+    	File conf = new File(service.getSyncDaemonHome(), "projectsConfig.xml");
+    	assertThat(conf.exists()).isTrue();
+    	ProjectCollection loadedProject = xmlMapper.readValue(conf, ProjectCollection.class);
+    	assertThat(loadedProject).isNotNull();
+    	assertThat(loadedProject.getProjects().size()).isEqualTo(1);
+    	assertThat(loadedProject.getProjects().get(0).getId()).isEqualTo(project.getId());
+    	assertThat(loadedProject.getProjects().get(0).getRevision()).isEqualTo(project.getRevision());
+    	assertThat(loadedProject.getProjects().get(0).getRootPath()).isEqualTo(project.getRootPath());
     }
 }
