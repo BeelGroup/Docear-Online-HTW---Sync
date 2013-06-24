81e0ea6642539e2f4dc41a6b12cdffcf5310c0b0
AlexSchwank <alschwank@gmail.com>
2013-06-24 11:31:19 +0200

    removed old classes.

--- src/main/java/org/docear/syncdaemon/projects/LocalProjectService.java
+++
@@ -1,9 +0,0 @@
-package org.docear.syncdaemon.projects;
-
-import java.util.List;
-
-public interface LocalProjectService {
-	
-	// return list of local projects
-    public List<Project> getProjects();
-}
