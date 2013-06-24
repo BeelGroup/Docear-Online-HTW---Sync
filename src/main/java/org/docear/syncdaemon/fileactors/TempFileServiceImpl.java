96bec8c0def795bc8ee3387181a724d92bc73f1b
AlexSchwank <alschwank@gmail.com>
2013-06-24 16:02:33 +0200

    optimized TempFileDetection. Added tests.

--- src/main/java/org/docear/syncdaemon/fileactors/TempFileServiceImpl.java
+++ src/main/java/org/docear/syncdaemon/fileactors/TempFileServiceImpl.java
@@ -4,17 +4,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.FilenameUtils;
 import org.docear.syncdaemon.NeedsConfig;
 import org.docear.syncdaemon.fileindex.FileMetaData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.typesafe.config.Config;
 
 public class TempFileServiceImpl implements TempFileService, NeedsConfig {
 	
 	private int timeOutMillis;
-	private List<String> tmpFileRegex;
+	private List<String> tmpFileExtensions;
 	
 	private Map<String, Long> timedOutFiles;
+	
+	private static final Logger logger = LoggerFactory.getLogger(TempFileServiceImpl.class);
 	
 	public TempFileServiceImpl() {
 		this.timedOutFiles = new HashMap<String, Long>();
@@ -30,24 +35,27 @@ public class TempFileServiceImpl implements TempFileService, NeedsConfig {
 		String path = filemetaData.getPath();
 		
 		// file already timed out long enough. The file is no temp file any more.
-		if (timedOutFiles.containsKey(path) && timedOutFiles.get(path) < (System.currentTimeMillis() + timeOutMillis)){
-			timedOutFiles.remove(path);
-			return false;
-		}
-		
-		for (String regex : tmpFileRegex){
-			if (filemetaData.getPath().matches(regex)){
-				timedOutFiles.put(filemetaData.getPath(), System.currentTimeMillis());
+		if (timedOutFiles.containsKey(path)){
+			if ((timedOutFiles.get(path) + timeOutMillis) < System.currentTimeMillis()){
+				timedOutFiles.remove(path);
+				return false;
+			} else {
 				return true;
 			}
+		} 
+		
+		if (FilenameUtils.isExtension(path, tmpFileExtensions)){
+			timedOutFiles.put(path, System.currentTimeMillis());
+			return true;		
 		}
+		
 		return false;
 	}
 
 	@Override
 	public void setConfig(Config config) {
-		this.timeOutMillis = config.getInt("daemon.tmpfiles.millis");
-		this.tmpFileRegex = config.getStringList("daemon.tmpfiles.regex");
+		this.timeOutMillis = config.getInt("daemon.tmpfiles.millis");	
+		this.tmpFileExtensions = config.getStringList("daemon.tmpfiles.regex");
 	}
 
 }
--- src/test/java/org/docear/syncdaemon/fileactors/TempFileServiceTest.java
+++ src/test/java/org/docear/syncdaemon/fileactors/TempFileServiceTest.java
@@ -1,18 +1,63 @@
 package org.docear.syncdaemon.fileactors;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+
+import org.docear.syncdaemon.fileindex.FileMetaData;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 public class TempFileServiceTest {
-
+	
+	private Config config;
+	private TempFileServiceImpl service;
+	
 	@Before
-    public void setUp(){
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        
-    }
-
+	public void setUp(){
+		config = ConfigFactory.parseString("daemon.tmpfiles.millis=500\ndaemon.tmpfiles.regex=[\"tmp\", \"BAK\",\"kate-swp\"]");
+		service = new TempFileServiceImpl();
+		service.setConfig(config);
+	}
+	
+	@After
+	public void tearDown(){
+		service = null;
+		config = null;
+	}
+	
+	@Test
+	public void testTempFileServiceRealFile(){
+		File file = new File("noTmpFile.mm");
+		FileMetaData fileMetaData = FileMetaData.newFile(file.getPath(), "thisIsTheHash", "thisIsTheProjectId");
+		
+		assertThat(service.isTempFile(fileMetaData)).isFalse();
+	}
+	
+	@Test
+	public void testTempFileService(){
+		File file = new File("tmpFile.tmp");
+		FileMetaData fileMetaData = FileMetaData.newFile(file.getPath(), "thisIsTheHash", "thisIsTheProjectId");
+		
+		assertThat(service.isTempFile(fileMetaData)).isTrue();
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertThat(service.isTempFile(fileMetaData)).isTrue();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertThat(service.isTempFile(fileMetaData)).isFalse();
+	}
+    
 }
